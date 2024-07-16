2ed8d066cc7ab0cb088ca956d5f4deefbddbf7ea
Gigi Ezieshvili <giorgiezie2004@gmail.com>
2024-07-16 19:11:37 +0400

    feat/CCC-39/Music-Module

---
+++ src/music/music.repository.ts
@@ -0,0 +1,53 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Music } from "./entities/music.entity";
+import { CreateMusicDto } from "./dto/create-music.dto";
+import { Repository } from "typeorm";
+import { UpdateMusicDto } from "./dto/update-music.dto";
+
+@Injectable()
+export class MusicRepository {
+    constructor(
+        @InjectRepository(Music)
+        private musicRepository: Repository<Music>
+    ) { }
+
+    create(data: CreateMusicDto){
+        const newMusic = this.musicRepository.create(data)
+
+        return this.musicRepository.save(newMusic)
+    }
+
+    findAll(){
+        return this.musicRepository
+            .createQueryBuilder("music")
+            .getMany()
+    }
+
+    findOne(id: number){
+        return this.musicRepository
+            .createQueryBuilder("music")
+            .where("music.id = :id", { id })
+            .getMany()
+    }
+
+    async update(id: number, data: UpdateMusicDto){
+        await this.musicRepository.createQueryBuilder("music")
+        .update()
+        .set(data)
+        .where("music.id = :id", {id})
+        .execute()
+
+        return this.musicRepository.findOneBy({id})
+    }
+
+    async remove(id: number){
+        await this.musicRepository.softDelete(id)
+
+        return this.musicRepository
+        .createQueryBuilder("music")
+        .withDeleted()
+        .where("music.id = :id", {id})
+        .getOne()
+    }
+}
