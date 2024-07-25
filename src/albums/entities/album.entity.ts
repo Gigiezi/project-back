20645a8512ec508ef89d5bdce15c8b6aaed399b5
Luka Shashiashvili <154319427+LukaShashiashvili@users.noreply.github.com>
2024-07-25 14:56:24 +0400

    Update album.entity.ts/ changed Album to AlbumEntity

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
-export class Album {
+export class AlbumEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
